22c4b36aca2b1e2b386b842343e7f14e0150cbda
Ezeki Zibzibadze <playerx@users.noreply.github.com>
2021-05-25 19:14:59 +0400

    Update mod.ts

--- src/mod.ts
+++ src/mod.ts
@@ -7,7 +7,7 @@ addEventListener("fetch", (event) => {
   const response = new Response(JSON.stringify({
     status: "✅ Started!",
     startedAt,
-    upTime: ((new Date().getTime() - startedAt.getTime()) / 1000*60*60).toFixed(1) + 'h.'
+    upTime: ((new Date().getTime() - startedAt.getTime()) / 1000*60*60*24).toFixed(1) + 'h.'
   }), { headers: { "content-type": "application/json" } })
   
   event.respondWith(response)
