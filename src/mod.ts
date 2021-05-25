fdbdeba2eb3802ece95c443df1e822380664f927
Ezeki Zibzibadze <playerx@users.noreply.github.com>
2021-05-25 19:16:53 +0400

    chore: updated console log message

--- src/mod.ts
+++ src/mod.ts
@@ -1,11 +1,11 @@
-console.log('started')
 
 const startedAt = new Date()
+
+console.log('✅ Microservice started', startedAt)
 
 
 addEventListener("fetch", (event) => {
   const response = new Response(JSON.stringify({
-    status: "✅ Started!",
     startedAt,
     upTime: ((new Date().getTime() - startedAt.getTime()) / 1000*60*60*24).toFixed(1) + 'h.'
   }), { headers: { "content-type": "application/json" } })
