6846f777de2ecb763836aea4d029f5f6b5f4c98d
Ezeki Zibzibadze <playerx@users.noreply.github.com>
2021-05-25 19:52:23 +0400

    Update mod.ts

--- src/mod.ts
+++ src/mod.ts
@@ -5,6 +5,8 @@ console.log('✅ Microservice started', startedAt)
 
 
 addEventListener("fetch", (event) => {
+  console.log('received event', event)
+  
   const response = new Response(JSON.stringify({
     startedAt,
     upTime: ((new Date().getTime() - startedAt.getTime()) / 1000*60*60*24).toFixed(1) + 'h.'
