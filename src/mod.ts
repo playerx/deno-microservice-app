68cd0dfddf5d8fd135af5bca7e1e8d33a2910394
Ezeki Zibzibadze <playerx@users.noreply.github.com>
2021-06-04 21:37:48 +0400

    Update mod.ts

--- src/mod.ts
+++ src/mod.ts
@@ -7,11 +7,11 @@ console.log('✅ Microservice started', startedAt)
 addEventListener("fetch", (event) => {
   console.log('WEBHOOK', event.request.url)
   
-  const response = new Response(JSON.stringify({
-    startedAt,
-    request: event.request,
-    upTime: ((new Date().getTime() - startedAt.getTime()) / 1000*60*60*24).toFixed(1) + 'h.'
-  }), { headers: { "content-type": "application/json" } })
+//   const response = new Response(JSON.stringify({
+//     startedAt,
+//     request: event.request,
+//     upTime: ((new Date().getTime() - startedAt.getTime()) / 1000*60*60*24).toFixed(1) + 'h.'
+//   }), { headers: { "content-type": "application/json" } })
   
-  event.respondWith(response)
+  event.respondWith(new Response('vc_success'))
 })
